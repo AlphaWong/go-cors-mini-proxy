52e3f02628d152d07a5155bd54a17f71d58e5640
alpha.wong <alpha.wong@lalamove.com>
2017-11-11 18:27:58 +0800

    ADD: main logic.

--- app.go
+++ app.go
@@ -3,9 +3,35 @@ package main
 import (
 	"net/http"
 	"log"
+	"net/http/httputil"
+	"net/url"
 )
 
+const backendServerURI = "https://postman-echo.com/"
+const port = ":80"
+
 func main() {
-	mux := http.NewServeMux()
-	log.Fatal(http.ListenAndServe(":2017", mux))
+	log.Printf("CORS server on %s", port)
+	uri, err := url.Parse(backendServerURI)
+	if err != nil {
+		log.Fatal("Invalid URI")
+	}
+	reverseProxy := httputil.NewSingleHostReverseProxy(uri)
+	h := middlewareCORS(reverseProxy)
+	h = middlewareSameHost(h)
+	log.Fatal(http.ListenAndServe(port, h))
+}
+
+func middlewareCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
+		next.ServeHTTP(w, r)
+	})
+}
+func middlewareSameHost(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Host = r.URL.Host
+		next.ServeHTTP(w, r)
+	})
 }
